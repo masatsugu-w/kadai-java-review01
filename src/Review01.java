2d2268a94f7dcf12c525728cda0ecdbd817ed3e0
masatsugu-w <watama0618@gmail.com>
2025-04-15 21:31:50 +0900

    メソッド名getSaleTax→taxへ変更

--- src/Review01.java
+++ src/Review01.java
@@ -3,8 +3,8 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1500;  //商品価格
-        int tax = 10;  //消費税率%
-        int salesTax = getSalesTax(price, tax);  //消費税
+        int consumptionTaxRate = 10;  //消費税率%
+        int salesTax = tax(price, consumptionTaxRate);  //消費税
         int incTax = price + salesTax;  //税込価格
        
         System.out.print(price + "円の");
@@ -12,8 +12,8 @@ public class Review01 {
         System.out.print("(消費税は" + salesTax + "円)です。");
     }
 
-    public static int getSalesTax(int price, int tax){
-        int result = price * tax / 100;
+    public static int tax(int price, int consumptionTaxRate){
+        int result = price * consumptionTaxRate / 100;
         return result;
         
     }
